8422f607e3a8dbe285c71e730c9f034fadebfc3e
et0o <akshatbisht78@gmail.com>
2024-06-12 23:10:53 +0530

    login page

--- script.js
+++ script.js
@@ -1,31 +1,11 @@
-const loginBtn = document.querySelector("#login");
-const registerBtn = document.querySelector("#register");
-const loginForm = document.querySelector(".login-form");
-const registerForm = document.querySelector(".register-form");
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () =>{
+    container.classList.add("active");
+});
 
 loginBtn.addEventListener('click', () => {
-    loginBtn.style.backgroundColor = "21264d";
-    registerBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
-
-    loginForm.style.left = "50%";
-    registerForm.style.left = "-50%";
-
-    loginForm.style.opacity = 1;
-    registerForm.style.opacity = 0;
-
-    document.querySelector("col-1").style.borderRadius = "0 30% 20% 0";
-})
-
-registerBtn.addEventListener('click', ()=> {
-    loginBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
-    registerBtn.style.backgroundColor = "21264d";
-
-    loginForm.style.left = "150%";
-    registerForm.style.left = "50%";
-
-    loginForm.style.opacity = 0;
-    registerForm.style.opacity = 1;
-
-    document.querySelector("col-1").style.borderRadius = " 0 20% 30% 0";
-
-})
+    container.classList.remove("active");
+});
